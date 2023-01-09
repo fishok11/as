5a8f4d503559d1159b94e6b9878483e0e1a69a9d
fishok11 <svllsd811@gmail.com>
2023-01-09 19:57:58 +0300

    mui ico

--- src/components/admin/ReturnToEdit.js
+++ src/components/admin/ReturnToEdit.js
@@ -5,14 +5,18 @@ import {
   RETURN_CREATE_GROUP_OWNER,
   RETURN_CREATE_YOUR_GIFT,
 } from "../../store/actions/actionTypes"
+import PeopleIcon from '@mui/icons-material/People';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import PersonIcon from '@mui/icons-material/Person';
+import RedeemIcon from '@mui/icons-material/Redeem';
 
 const ReturnToEdit = () => {
   return (
     <div className="ReturnToEdit-container">
-      <Step number="1" action={RETURN_CREATE_GROUP_NAME}/>
-      <Step number="2" action={RETURN_CREATE_EVENT_DATE}/>
-      <Step number="3" action={RETURN_CREATE_GROUP_OWNER}/>
-      <Step number="4" action={RETURN_CREATE_YOUR_GIFT}/>
+      <Step ico={<PeopleIcon />} action={RETURN_CREATE_GROUP_NAME}/>
+      <Step ico={<CalendarMonthIcon />} action={RETURN_CREATE_EVENT_DATE}/>
+      <Step ico={<PersonIcon />} action={RETURN_CREATE_GROUP_OWNER}/>
+      <Step ico={<RedeemIcon />} action={RETURN_CREATE_YOUR_GIFT}/>
     </div>
   )
 }
--- src/components/admin/Step.js
+++ src/components/admin/Step.js
@@ -2,17 +2,17 @@ import { useDispatch } from "react-redux";
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-const Step = ({number, action}) => {
+const Step = ({ico, action}) => {
   const dispatch = useDispatch()
 
   return (
     <Button
-      variant="contained"   
+      variant="text"   
       disableElevation
       sx={{ borderRadius: '50%', minWidth: 0, width: 35, height: 35, p: '8px'}}
       onClick={() => dispatch({type: action})}
     >
-      <Typography variant="button" >{number}</Typography>
+      <Typography variant="button" display='block'>{ico}</Typography>
     </Button>
   )
 };
