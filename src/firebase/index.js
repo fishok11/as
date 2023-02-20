fa290be3dd8a9863c0df21f6bb53db02f7882dfb
fishok11 <svllsd811@gmail.com>
2023-02-20 21:44:52 +0300

    firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBh-KjNE_LE0tZxQL5YuC13k4J9m27nWHQ",
+  authDomain: "anonymous-santa-3f0dd.firebaseapp.com",
+  projectId: "anonymous-santa-3f0dd",
+  storageBucket: "anonymous-santa-3f0dd.appspot.com",
+  messagingSenderId: "426333061494",
+  appId: "1:426333061494:web:2af772567e8f730c603a57",
+  measurementId: "G-GVCSLYZR6K" 
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
